9529fa0041de1aacd8194aff8a4ff93d7f895068
Tim Hanewich <tahanewich@live.com>
2022-01-18 15:23:52 -0500

    Added fields parameter to RateRequest

--- Virtual-Tables-Demo/RestApiAzureFunction/HttpTrigger1.cs
+++ Virtual-Tables-Demo/RestApiAzureFunction/HttpTrigger1.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.Logging;
 using CoreCode;
 using System.IO;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Web;
+using System;
+using System.Collections.Specialized;
 
 namespace VirtualTablesDemo
 {
@@ -15,13 +19,35 @@ namespace VirtualTablesDemo
         [Function("RateRequest")]
         public static HttpResponseData RateRequest([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req, FunctionContext context)
         {
+            //Params
+            //fields - only include certain fields. for example, fields=Id,DateFiled
+
             ILogger log = context.GetLogger("RateRequest");
             log.LogInformation("RateRequest call received!");
 
+            //Get fields param
+            NameValueCollection nvc = HttpUtility.ParseQueryString(req.Url.ToString());
+            string ip_fields = nvc.Get("fields");
+            string[] OnlyIncludeFields = null;
+            if (ip_fields != null)
+            {
+                OnlyIncludeFields = ip_fields.Split(new string[]{","}, StringSplitOptions.None);
+            }
+
+            //Get the JSON string to return
+            string JsonToReturn = null;
+            if (OnlyIncludeFields == null)
+            {
+                JsonToReturn = CoreCode.RateRequest.ToJson(CoreCode.RateRequest.All());
+            }
+            else
+            {
+                JsonToReturn = CoreCode.RateRequest.ToJson(CoreCode.RateRequest.All(), OnlyIncludeFields);
+            }
             
             HttpResponseData ToReturn = req.CreateResponse();
             ToReturn.StatusCode = HttpStatusCode.OK;
-            ToReturn.WriteString(CoreCode.RateRequest.ToJson(CoreCode.RateRequest.All()));
+            ToReturn.WriteString(JsonToReturn);
             ToReturn.Headers.Add("Content-Type", "application/json");
             return ToReturn;
             
