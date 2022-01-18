c457554920a0d25d79dddd7774c88ac13b20487d
Tim Hanewich <tahanewich@live.com>
2022-01-18 13:04:02 -0500

    Added CoreCode

---
+++ Virtual-Tables-Demo/CoreCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
