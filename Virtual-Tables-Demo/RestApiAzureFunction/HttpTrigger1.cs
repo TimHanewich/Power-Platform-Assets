819bff3e637fc2645a8542a63f189ef56bd8392d
Tim Hanewich <tahanewich@live.com>
2022-01-20 10:40:45 -0500

    Added sample api

--- Virtual-Tables-Demo/RestApiAzureFunction/HttpTrigger1.cs
+++ Virtual-Tables-Demo/RestApiAzureFunction/HttpTrigger1.cs
@@ -138,5 +138,80 @@ namespace VirtualTablesDemo
 
         }
 
+        [Function("sample")]
+        public static async Task<HttpResponseData> sample([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "sample/{table?}")] HttpRequestData req, FunctionContext context, string table)
+        {
+            ILogger log = context.GetLogger("sample");
+
+
+            if (table == null)
+            {
+                log.LogInformation("Table not provided. Will provide directory");
+                JObject ToReturn = new JObject();
+                ToReturn.Add("@odata.context", "https://nmosi2.azurewebsites.net/sample/$metadata");
+                JObject jo = new JObject();
+                jo.Add("name", "Advertisements");
+                jo.Add("kind", "EntitySet");
+                jo.Add("url", "Advertisements");
+                JArray ToAddJAr = JArray.Parse(JsonConvert.SerializeObject(new JObject[] {jo}));
+                ToReturn.Add("value", ToAddJAr);
+
+                HttpResponseData data = req.CreateResponse();
+                data.StatusCode = HttpStatusCode.OK;
+                data.WriteString(ToReturn.ToString());
+                data.Headers.Add("Content-Type", "application/json");
+                return data;
+            }
+            else if (table == "$metadata")
+            {
+                log.LogInformation("Metadata was requested.");
+                HttpClient hc = new HttpClient();
+                HttpResponseMessage resp = await hc.GetAsync("https://nmosi.blob.core.windows.net/general/SampleMetadata.xml?sp=r&st=2022-01-20T15:31:30Z&se=2024-10-03T22:31:30Z&sv=2020-08-04&sr=b&sig=0Eq6dQhEic%2FJM4BzPHNtUughlpIcuQesh4jF9uVG%2F68%3D");
+                string content = await resp.Content.ReadAsStringAsync();
+                HttpResponseData ToResp = req.CreateResponse();
+                ToResp.WriteString(content);
+                ToResp.Headers.Add("Content-Type", "application/xml");
+                return ToResp;
+            }
+            else if (table == "Advertisements")
+            {
+                
+                //Item 1
+                JObject jo1 = new JObject();
+                jo1.Add("@odata.mediaReadLink", "Advertisements(f89dee73-af9f-4cd4-b330-db93c25ff3c7)/$value");
+                jo1.Add("@odata.mediaContentType", "*/*");
+                jo1.Add("@odata.mediaEtag", "\"8zOOKKvgOtptr4gt8IrnapX3jds=\"");
+                jo1.Add("ID", Guid.Parse("f89dee73-af9f-4cd4-b330-db93c25ff3c7"));
+                jo1.Add("Name", "Old School Lemonade Store, Retro Style");
+                jo1.Add("AirDate", new DateTime(2012, 11, 7));
+
+                //Item 1
+                JObject jo2 = new JObject();
+                jo2.Add("@odata.mediaReadLink", "Advertisements(db2d2186-1c29-4d1e-88ef-a127f521b9c6)/$value");
+                jo2.Add("@odata.mediaContentType", "*/*");
+                jo2.Add("ID", Guid.Parse("db2d2186-1c29-4d1e-88ef-a127f521b9c6"));
+                jo2.Add("Name", "Early morning start, need coffee");
+                jo2.Add("AirDate", new DateTime(2000, 2, 29));
+
+                JObject ToReturn = new JObject();
+                ToReturn.Add("@odata.context", "https://nmosi2.azurewebsites.net/sample/$metadata");
+                ToReturn.Add("value", JArray.Parse(JsonConvert.SerializeObject(new JObject[]{jo1, jo2})));
+
+                HttpResponseData resp = req.CreateResponse();
+                resp.StatusCode = HttpStatusCode.OK;
+                resp.WriteString(ToReturn.ToString());
+                resp.Headers.Add("Content-Type", "application/json");
+                return resp; 
+            }
+            else
+            {
+                HttpResponseData resp = req.CreateResponse();
+                resp.StatusCode = HttpStatusCode.BadRequest;
+                resp.WriteString("Table '" + table + "' is invalid.");
+                return resp;
+            }
+        }
+
+
     }
 }
