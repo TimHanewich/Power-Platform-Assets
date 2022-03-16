ba2807d6386e96b8b932837cb6a7e5b583913cef
Tim Hanewich <tahanewich@live.com>
2022-03-16 15:10:34 -0400

    Added CDS authentication

--- Demos/Corrections-Facility-AI/CoreLibrary/FaceAuthenticator.cs
+++ Demos/Corrections-Facility-AI/CoreLibrary/FaceAuthenticator.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 using Microsoft.Azure.CognitiveServices.Vision.Face;
 using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using TimHanewich.Cds;
 
 namespace CoreLibrary
 {
@@ -17,6 +18,19 @@ namespace CoreLibrary
             IFaceClient client = new FaceClient(new ApiKeyServiceClientCredentials("b41d3e97004f4c78a30c79596187eb0e"));
             client.Endpoint = "https://testfaceapi20220311.cognitiveservices.azure.com";
             return client;
+        }
+
+        public static async Task<CdsService> AuthenticateCDSAsync()
+        {
+            CdsAuthenticator auth = new CdsAuthenticator();
+            auth.Username = "USERNAME";
+            auth.Password = "PASSWORD";
+            auth.Resource = "https://org007274b9.crm.dynamics.com/";
+            auth.ClientId = Guid.Parse("51f81489-12ee-4a9e-aaae-a2591f45987d");
+            await auth.GetAccessTokenAsync();
+
+            CdsService service = new CdsService(auth.Resource, auth.AccessToken);
+            return service;
         }
     }
 }
