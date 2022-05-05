f97ccce794533e416572c7904fa2f1566ca7c0b8
Tim Hanewich <tahanewich@live.com>
2022-05-05 13:31:24 -0600

    Added SqlCredentialsProvider

---
+++ Demos/Dataverse-Performance/src/SqlCredentialsProvider.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DataversePerformance
+{
+    public class SqlCredentialsProvider
+    {
+        public static string GetSqlConnectionString()
+        {
+            return "";
+        }
+    }
+}
