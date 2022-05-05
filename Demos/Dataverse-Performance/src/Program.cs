8cca00f29c3deb9f888ceae7995057281031bd0f
Tim Hanewich <tahanewich@live.com>
2022-05-05 12:36:36 -0600

    Added percent complete

--- Demos/Dataverse-Performance/src/Program.cs
+++ Demos/Dataverse-Performance/src/Program.cs
@@ -23,8 +23,10 @@ namespace DataversePerformance
 
             for (int t = 0; t < contacts.Length; t++)
             {
+                float pc = Convert.ToSingle(t) / Convert.ToSingle(contacts.Length);
+
                 Contact toupload = contacts[t];
-                Console.Write("Uploading # " + (t+1).ToString("#,##0") + " / " + contacts.Length.ToString("#,##0") + "... ");
+                Console.Write("Uploading # " + (t+1).ToString("#,##0") + " / " + contacts.Length.ToString("#,##0") + " (" + pc.ToString("#0.0%") + ")... ");
                 await cds.CreateRecordAsync("contacts", toupload.ToDataversePayload().ToString());
                 Console.WriteLine("Success!");
             }
