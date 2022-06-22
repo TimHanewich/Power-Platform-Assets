9ea0455edb22e9f253573dbf67a904a709c83a87
Tim Hanewich <tahanewich@live.com>
2022-06-22 09:19:23 -0400

    Added Dataverse Storage estimator

---
+++ applications/dataverse-storage-estimator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TimHanewich.Cds;
+using TimHanewich.Cds.Metadata;
+
+namespace DataverseStorageEstimator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
