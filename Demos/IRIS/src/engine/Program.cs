6d99b9aa7120380f6345715fbf8617b3421d101b
Tim Hanewich <tahanewich@live.com>
2023-10-09 11:27:39 -0400

    Removed test

--- Demos/IRIS/src/engine/Program.cs
+++ Demos/IRIS/src/engine/Program.cs
@@ -10,10 +10,7 @@ namespace PSJ
     {
         public static void Main(string[] args)
         {            
-            CaseTestimony[] testimonies = InvestigationTools.RetrieveAndPrepareTestimoniesAsync(new Guid[]{Guid.Parse("7cf84f84-f211-ee11-8f6d-0022481c8ac0"), Guid.Parse("29ff0b17-f311-ee11-8f6d-0022481c8ac0")}).Result;
-            string comp = InvestigationTools.CompareTestimoniesAsync(testimonies).Result;
-            Console.WriteLine(comp);
-
+            Console.WriteLine("hi!");
         }
     }
 }
